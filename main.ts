a0b1c8abf15773fe233d9328f572409f7277855f
davidr <davidrowthorn@gmail.com>
2021-01-23 13:52:35 +0000

    Added length class

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 class Energy {
-  calories: number;
+  public calories: number;
 
   constructor(calories: number) {
     this.calories = calories;
@@ -10,10 +10,46 @@ class Energy {
   }
 }
 
-class Height {
+class Length {
+  public cm: number;
+  units = {
+    in: 2.54,
+    mile: 160934.4,
+    m: 100,
+    km: 1000,
+  };
+
+  constructor(cm: number) {
+    this.cm = cm;
+  }
+
+  add(l: Length): Length {
+    return new Length(this.cm + l.cm);
+  }
+
+  subtract(l: Length): Length {
+    return new Length(this.cm - l.cm);
+  }
+
+  multiplyBy(l: Length): Length {
+    return new Length(this.cm * l.cm);
+  }
+
+  divideBy(l: Length): Length {
+    return new Length(this.cm / l.cm);
+  }
 }
 
 class Person {
+  public height;
+  public weight;
+  public personalConstant;
+
+  constructor(height: Length, weight: Weight, personalConstant: number) {
+    this.height = height;
+    this.weight = weight;
+    this.personalConstant = personalConstant;
+  }
 }
 
 class Route {
@@ -26,6 +62,16 @@ class Stretch {
 }
 
 class Weight {
+  public kg: number;
+
+  constructor(kg: number) {
+    this.kg = kg;
+  }
+}
+
+class CalorieCalculator {
+  calculate(person: Person, stretch: Stretch, surface: Surface) {
+  }
 }
 
 export { Energy };
